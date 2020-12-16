4183ebbbc446975ff059c6ab9d467fde267c1230
Dv <dv@Dvs-MacBook-Pro.local>
2020-12-16 20:27:27 +0530

    review using react

---
+++ src/Review.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+const [counter, setCounter] = useState(0);
+const {id, name, job, image, text} = people[counter];
+
+const check = (number) => {
+  if(number > people.length -1){
+    return  0;
+  }
+  if(number < 0){
+    return people.length -1 ;
+  }
+  return number;
+};
+
+const nextPerson = ()=>{
+  setCounter((counter) =>{
+    let newCounter = counter + 1;
+    return check(newCounter);
+  });
+};
+
+const prevPerson = () =>{
+  setCounter((counter) => {
+    let newCounter = counter - 1;
+    return check(newCounter);
+  });
+};
+
+const randomPerson = () => {
+  let randomNumber = Math.floor(Math.random() * people.length);
+  if(randomNumber === counter){
+    randomNumber = counter + 1;
+  }
+  setCounter(check(randomNumber));
+}
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn' onClick={randomPerson}>
+        surprise me
+      </button>
+    </article>
+  ); 
+};
+
+export default Review;
